7411b505f35636dcf30d07d29c75ac672cf4df95
Rishie13 <mehtarishie1@gmail.com>
2017-04-10 17:54:32 -0400

    Initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+var app = angular.module("profile",['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider){ 
+  $routeProvider.
+  when('/main',{
+    templateUrl: 'main.html',
+    controller: 'MainCtrl'
+  }).
+    when('/dervices',{
+    templateUrl: 'dervices.html',
+    controller: 'DervicesCtrl'
+  }).
+    when('/about',{
+    templateUrl: 'about.html',
+    controller: 'AboutCtrl'
+  }).
+    when('/contacts',{
+    templateUrl: 'contacts.html',
+    controller: 'ContactsCtrl'
+  }).
+  otherwise({redirectTo:'/main'});
+}])
+
+.controller('MainCtrl', ['$scope', function($scope){
+ 
+}])
+
+.controller('AboutCtrl', ['$scope', '$http', function($scope, $http){
+    $http.get('about.json').then(function(response){
+     $scope.about=response.data;
+    });
+}])
+
+.controller('DervicesCtrl', ['$scope', '$http', function($scope, $http){
+    $http.get('dervices.json').then(function(response){
+     $scope.dervices=response.data;
+    });
+}])
+
+.controller('ContactsCtrl', ['$scope', function($scope){
+ 
+}]);
+
